3d7f8f9284ad88575db7d886220bfa7af34041ee
Nepcam <cameron.nepe@gmail.com>
2018-04-17 21:20:17 +1200

    make two green8

--- events.js
+++ events.js
@@ -43,3 +43,7 @@ function makeBlue (evt) {
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
 }
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
+}
